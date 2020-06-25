bbf592325c1018ff3fd624dd610e9d1aa0e6c303
hyoilll <50327128+hyoilll@users.noreply.github.com>
2020-06-26 01:01:30 +0900

    2020/06/26 express server open

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("hello");
+const express = require("express");
+//require - node module(express)를 include하는 개념
+const app = express();
+
+const PORT = 4000;
+
+function handleListening() {
+  console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+function handleHome(req, res) {
+  res.send("shut up");
+}
+
+function handleProfile(req, res) {
+  res.send("You are on my profile");
+}
+
+//server에 요청
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+//PORT에 접속하면, handleListening을 실행하라
+app.listen(PORT, handleListening);
